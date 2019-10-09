b9040c1b9457bdc1a57aeec61ae4922f49fcea4e
Varun Resh Maddali <mvresh@gmail.com>
2019-10-09 16:55:04 +0530

    completed the exercise

--- lib/story.dart
+++ lib/story.dart
@@ -1,3 +1,13 @@
 //TODO: Step 2 - Create a new class called Story with storyTitle, choice1 and choice2 fields
+class Story{
+  String storyTitle;
+  var choice1;
+  var choice2;
 
+  Story(storyTitle,choice1,choice2){
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
 // TODO: Step 3 - Create constructor with named parameters to initialise these properties
